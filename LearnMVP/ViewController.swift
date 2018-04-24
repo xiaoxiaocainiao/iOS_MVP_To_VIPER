9fd5add5d530a3a3542fc505cfb0ce0f5a527e55
小小菜鸟 <wangyan.work@qq.com>
2018-04-24 09:28:35 +0800

    Initial Commit

---
+++ LearnMVP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LearnMVP
+//
+//  Created by 王雁 on 2018/4/24.
+//  Copyright © 2018年 王雁. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
